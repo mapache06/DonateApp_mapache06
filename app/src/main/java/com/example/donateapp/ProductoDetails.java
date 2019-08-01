a264c465cca38726735ce09a69b6cb74cd8ab69c
mapache06 <301710709@bis.ute.edu.mx>
2019-08-01 00:26:05 -0500

    se hizo pantalla para visualizar un artículo

---
+++ app/src/main/java/com/example/donateapp/ProductoDetails.java
@@ -0,0 +1,13 @@
+package com.example.donateapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductoDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto_details);
+    }
+}
